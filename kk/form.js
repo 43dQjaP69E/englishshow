647a3df35fca63ac530ba9b75c52e0ec9f526c12
43dQjaP69E <94623999+43dQjaP69E@users.noreply.github.com>
2021-11-18 23:39:48 +0300

    Create form.js

---
+++ kk/form.js
@@ -0,0 +1,14 @@
+let email = document.querySelector('.kk-email');
+let emailLabel = document.createElement('label');
+emailLabel.textContent = 'email';
+email.prepend(emailLabel);
+
+let phone = document.querySelector('.kk-phone');
+let phoneLabel = document.createElement('label');
+phoneLabel.textContent = 'номер телефона';
+phone.prepend(phoneLabel);
+
+let name = document.querySelector('.kk-name');
+let nameLabel = document.createElement('label');
+nameLabel.textContent = 'ваше имя';
+name.prepend(nameLabel);
