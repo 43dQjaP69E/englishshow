752d73591be04146c5e7a3b8b3423ca1f1026c90
43dQjaP69E <94623999+43dQjaP69E@users.noreply.github.com>
2021-11-18 18:28:23 +0300

    Add files via upload

--- ns/index.js
+++ ns/index.js
@@ -1,3 +1,43 @@
+const url = window.location.href;
+
+let csv = url.match(/chatsovsemi-\d/);
+let ssp = url.match(/samsebeprebod-\d/);
+
+function month(num) {
+    if (num == 1) {
+        console.log('месяц')
+    	return '';
+    }
+    if (num == 3) {
+    	return 'a';
+    };
+    if (num >= 6) {
+    	return 'ев';
+    };
+};
+
+if (csv || ssp) {
+    const $prod = document.querySelector('.prod');
+    if (csv) {
+    	let num = csv[0].match(/\d/);
+		let end = month(num);
+        $prod.innerHTML = `
+            <p class="prod__title">Native Show</p>
+            <p class="prod__type">#чатсовсеми</p>
+			<p class="prod__dur">${num} месяц${end}</p>
+		`;
+    };
+    if (ssp) {
+    	let num = ssp[0].match(/\d/);
+        let end = month(num);
+        $prod.innerHTML = `
+            <p class="prod__title">Native Show</p>
+            <p class="prod__type">#самсебепрепод</p>
+			<p class="prod__dur">${num} месяц${end}</p>
+		`;
+    };
+};
+
 const placeholder = {
     name: 'Имя',
     email: 'Email',
