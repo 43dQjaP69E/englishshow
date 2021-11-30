78b6f47260a85ce5b922bb7136ca064ef1f8003a
43dQjaP69E <94623999+43dQjaP69E@users.noreply.github.com>
2021-11-30 16:12:46 +0300

    Add files via upload

---
+++ crd/index.js
@@ -0,0 +1,43 @@
+const cards$ = {
+    '1': '2 490',
+    '2': '4 200'
+};
+const url = window.location.href;
+const num = url.match(/\d/);
+const $prod = document.querySelector('.prod');
+const $prodprice = document.querySelector('.prod__price');
+
+if (num) {
+    if (num === 1) {
+        $prod.innerHTML = `
+            <p class='prod__title'>Английский в карточках</p>
+            <p class='prod__type'>Идиомы</p>
+        `;
+    };
+
+    if(num === 2) {
+        $prod.innerHTML = `
+            <p class='prod__title'>Английский в карточках</p>
+            <p class='prod__type'>Идиомы&mdash;С другом дешевле</p>
+        `;
+    };
+
+    $prodprice.innerHTML = `К оплате: ${cards$[num]}`
+};
+
+const placeholder = {
+    name: 'Имя',
+    email: 'Email',
+    phone: 'Телефон'
+};
+
+const $n = document.getElementsByName('formParams[full_name]')[0];
+const $e = document.getElementsByName('formParams[email]')[0];
+const $p = document.getElementsByName('formParams[phone]')[0];
+
+$n.placeholder = `${placeholder.name}`;
+$e.placeholder = `${placeholder.email}`;
+$p.placeholder = `${placeholder.phone}`;
+
+$payButton = document.querySelector('.f-btn');
+$payButton.innerText = 'Оплатить';
